a01518b7d1c497ae97217e5ff0996f41ec536df8
dttinh18it3 <44200641+dttinh18it3@users.noreply.github.com>
2018-11-06 16:24:23 +0700

    Update MangConTro.cpp

--- MangConTro.cpp
+++ MangConTro.cpp
@@ -1,21 +1,34 @@
 #include<stdio.h>
 #include<conio.h>
 #include<malloc.h>
+//Nhap
 void NhapMang (int *Arr, int N)
 {
 	for(int i=0; i<N ; i++){
 		printf("arr[%d] =",i);
 		scanf("%d",Arr+i);
 	}
+	printf("\n");
 }
 
-void InMang	(int * arr, int n)
+//In
+void InMang	(int *arr, int n)
 {
 	for(int i=0; i<n ; i++){
 		printf("arr[%d]=%d\n",i,*(arr+i));
 	}
 
 }	
+
+//Chuyen doi
+void ChuyenDoi(int *arr, int *mang, int n)
+{
+	for(int i=0; i<n; i++){
+		*(mang+i)=*(arr+i);
+	}
+	printf("\n");
+}
+
 int main(){
 //nhap so n
 	int n,a;
@@ -23,6 +36,9 @@ int main(){
 	scanf("%d", &n);
 //cap bo nho cho con tro
 	int *p = (int *)malloc(n*sizeof(int));
-	NhapMang(p, n);
+	NhapMang(p,n);
 	InMang(p,n);
+	int *q = (int *)malloc(n*sizeof(int));
+	ChuyenDoi(p,q,n);
+	InMang(q,n);
 }
